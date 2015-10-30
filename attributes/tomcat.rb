fa6276b976deded94ab7ee5b09d4e19891f8d2e0
Martijn van der Woud <martijn@crimsoncricket.nl>
2015-10-30 09:39:22 +0100

    Use a non-blocking entropy source to prevent insanely slow startups in Tomcat.

--- attributes/tomcat.rb
+++ attributes/tomcat.rb
@@ -13,5 +13,5 @@
 # limitations under the License.
 
 override['tomcat']['deploy_manager_apps'] = false
-override['tomcat']['java_options'] = '-Djava.awt.headless=true -Xmx512m -XX:+UseConcMarkSweepGC'
-override['tomcat']['tomcat_auth'] = false
+override['tomcat']['java_options'] = '-Djava.awt.headless=true -Xmx512m -XX:+UseConcMarkSweepGC -Djava.security.egd=file:/dev/./urandom'
+override['tomcat']['tomcat_auth'] = falseUsU
