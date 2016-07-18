c30915fdba6c1844876eccdd3b89335c9e63f778
Martijn van der Woud <martijn@crimsoncricket.nl>
2016-07-18 13:15:36 +0200

    Upgrade to mysql 5.7 on Ubuntu 16.04 LTS

--- recipes/database.rb
+++ recipes/database.rb
@@ -20,7 +20,7 @@ database_root_password = data_bag_item('credentials', node['cc-webapp']['hostnam
 database_password = data_bag_item('credentials', node['cc-webapp']['hostname'])['mysql_webapp_password']
 
 mysql_service 'default' do
-  version '5.6'
+  version '5.7'
   bind_address '0.0.0.0'
   initial_root_password database_root_password
   action [:create, :start]
