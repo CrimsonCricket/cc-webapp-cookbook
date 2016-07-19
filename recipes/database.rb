920747efd58bc3f3627cb09aac849b742ce4f82a
Martijn van der Woud <martijn@crimsoncricket.nl>
2016-07-20 01:21:09 +0200

    Make mysql version to enable Ubuntu downgrade if necessary

--- recipes/database.rb
+++ recipes/database.rb
@@ -20,7 +20,7 @@ database_root_password = data_bag_item('credentials', node['cc-webapp']['hostnam
 database_password = data_bag_item('credentials', node['cc-webapp']['hostname'])['mysql_webapp_password']
 
 mysql_service 'default' do
-  version '5.7'
+  version node['cc-webapp']['database']['mysql_version']
   bind_address '0.0.0.0'
   initial_root_password database_root_password
   action [:create, :start]
