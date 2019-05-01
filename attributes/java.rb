41548108a5b02f6fa17bef670746b6c4468895c8
Martijn van der Woud <martijn@crimsoncricket.nl>
2019-05-01 10:39:03 +0200

    Switch to AdoptOpenJDK distribution for Java 11

--- attributes/java.rb
+++ attributes/java.rb
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-override['java']['install_flavor'] = 'openjdk'
-override['java']['jdk_version'] = 11
-override['java']['oracle']['accept_license_agreement'] = true
+default['java']['install_flavor'] = 'adoptopenjdk'
+default['java']['jdk_version'] = 11
+default['java']['adoptopenjdk']['variant'] = 'hotspot'
